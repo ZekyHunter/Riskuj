95bddeee114a740283b65ce938476d5f0e4a8343
Matěj Růžička <ja.matejruzicka@gmail.com>
2024-11-24 00:55:30 +0100

    wip - add gameboard, no modal

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,10 @@
 import axios from "axios";
 import React, { useState, Component } from "react";
 import UserBoard from "./UserBoard";
-//import GameBoard from "./GameBoard";
+import GameBoard from "./GameBoard";
 //import Modal from "./Modal";
+
+
 
 export default class App extends Component {
   constructor(props){
@@ -13,7 +15,14 @@ export default class App extends Component {
       setIsModalOpen: false,
       selectedQuestion: null,
       setSelectedQuestion: null,
-
+      categories: ["Historie", "Věda", "Kultura", "Sport", "Příroda"],
+      questions: [
+        ["Otázka 1", "Otázka 2", "Otázka 3", "Otázka 4", "Otázka 5"],
+        ["Otázka 6", "Otázka 7", "Otázka 8", "Otázka 9", "Otázka 10"],
+        ["Otázka 11", "Otázka 12", "Otázka 13", "Otázka 14", "Otázka 15"],
+        ["Otázka 16", "Otázka 17", "Otázka 18", "Otázka 19", "Otázka 20"],
+        ["Otázka 21", "Otázka 22", "Otázka 23", "Otázka 24", "Otázka 25"],
+      ],
     }
   }
 
@@ -44,10 +53,21 @@ export default class App extends Component {
             <UserBoard key={user.id} userName={user.name} userPoints={user.points}/>
           ))}
         </div>
-      </div>
+
+        <br/>
+
+        <GameBoard
+          categories={this.state.categories}
+          questions={this.state.questions}
+        />
+
+        </div>
     );
   };
 
 };
+
+
+
 
 
--- frontend/src/GameBoard.js
+++ frontend/src/GameBoard.js
@@ -1,42 +1,27 @@
 import React from "react";
 import "./GameBoard.css";
 
-//const GameBoard = ({ categories, questions, onQuestionClick }) => {
-//  return (
-//    <div className="game-board">
-//      {categories.map((category, catIndex) => (
-//        <React.Fragment key={catIndex}>
-//          <div className="category-cell">{category}</div>
-//          {questions[catIndex].map((question, qIndex) => (
-//            <div
-//              className="question-cell"
-//              key={qIndex}
-//              onClick={() => onQuestionClick(question)} // Předání otázky do modálního okna
-//            >
-//              {qIndex * 100 + 100}{" "}
-//              {/* Zobrazení čísla otázky (100, 200, 300, ...) */}
-//            </div>
-//          ))}
-//        </React.Fragment>
-//      ))}
-//    </div>
-//  );
-//};
-//
-//export default GameBoard;
 
-//export default function GameBoard({categories, questions, onQuestionClick}) {
-//  const categoryList = categories.map(category => <li>{category}</li>);
-//  return (
-//    <div className="game-board">
-//            <ul>{categoryList}</ul>
-//
-//    </div>
-//  );
-//}
-
-export default function GameBoard({categories, questions, onQuestionClick}) {
-  return (
-    <div className="game-board">Hello!</div>
+export default function GameBoard({categories, questions}) {
+  const categoryList = categories.map((category, mapIndex) =>
+    <>
+      <div className="category-cell">{category}</div>
+      {questions[mapIndex].map((question, questionIndex) =>
+        <div
+          className="question-cell"
+          key={questionIndex}
+          //onClick={() => onQuestionClick(question)}
+        >
+          {questionIndex * 100 + 100}{" "}
+        </div>
+      )}
+    </>
   );
-}
+
+  return (
+    <div className="game-board">
+      {categoryList}
+    </div>
+  );
+
+};
