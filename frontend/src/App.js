c2703fbfd602f623fbda2a969d5bfc0a19e1705b
MartyRoule <marty.roule@gmail.com>
2024-11-26 15:25:01 +0100

    wip

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,36 +4,47 @@ import React, { useState, Component } from "react";
 import UserBoard from "./UserBoard";
 import GameBoard from "./GameBoard";
 import Modal from "./Modal";
+import PlayerPage from "./PlayerPage";
 
 export default class App extends Component {
-  constructor(props){
-    super(props)
+  constructor(props) {
+    super(props);
     this.state = {
       users: [],
       isModalOpen: false,
       selectedQuestion: null,
-      categories: ["Historie", "Věda", "Kultura", "Sport", "Příroda", "Cokoliv"],
+      categories: [
+        "Historie",
+        "Věda",
+        "Kultura",
+        "Sport",
+        "Příroda",
+        "Cokoliv",
+      ],
       questions: [
         ["Otázka 1", "Otázka 2", "Otázka 3", "Otázka 4", "Otázka 5"],
         ["Otázka 6", "Otázka 7", "Otázka 8", "Otázka 9", "Otázka 10"],
         ["Otázka 11", "Otázka 12", "Otázka 13", "Otázka 14", "Otázka 15"],
         ["Otázka 16", "Otázka 17", "Otázka 18", "Otázka 19", "Otázka 20"],
         ["Otázka 21", "Otázka 22", "Otázka 23", "Otázka 24", "Otázka 25"],
+        ["Otázka 26", "Otázka 27", "Otázka 28", "Otázka 29", "Otázka 30"],
       ],
-    }
+    };
   }
 
-  changeModalState(question){
+  changeModalState(question) {
     this.setState({
       selectedQuestion: question,
-      isModalOpen: this.state.isModalOpen ? false : true
+      isModalOpen: this.state.isModalOpen ? false : true,
     });
-  };
+  }
 
-  getUsers(){
+  getUsers() {
     axios
       .get("/api/users/")
-      .then(res => {this.setState({users: res.data})})
+      .then((res) => {
+        this.setState({ users: res.data });
+      })
       .catch((err) => console.log(err));
   }
 
@@ -41,10 +52,24 @@ export default class App extends Component {
     return (
       <div>
         <h1 className="riskuj">Riskuj!</h1>
+        <div>
+          <h2>Jmeno</h2>
+          <p>72</p>
+        </div>
+
+        <div>
+          <h2>Jmeno</h2>
+          <p>72</p>
+        </div>
+
+        <div>
+          <h2>Jmeno</h2>
+          <p>72</p>
+        </div>
 
         <div id="users">
           <button onClick={() => this.getUsers()}>Get users</button>
-          {this.state.users.map(user => (
+          {this.state.users.map((user) => (
             <UserBoard
               key={user.id}
               userName={user.name}
@@ -53,7 +78,7 @@ export default class App extends Component {
           ))}
         </div>
 
-        <br/>
+        <br />
 
         <GameBoard
           categories={this.state.categories}
@@ -67,22 +92,7 @@ export default class App extends Component {
           question={this.state.selectedQuestion}
           changeModalState={this.changeModalState.bind(this)}
         />
-
-        <Router>
-          <Routes>
-            {/* Definice různých stránek */}
-            <Route path="/" element={<GameBoardPage />} /> {/* Hlavní stránka */}
-            <Route path="/player" element={<PlayerPage />} /> {/* Stránka hráče */}
-          </Routes>
-        </Router>
-
       </div>
     );
-  };
-
-};
-
-
-
-
-
+  }
+}
--- frontend/src/GameBoard.js
+++ frontend/src/GameBoard.js
@@ -1,28 +1,27 @@
 import React from "react";
 import "./GameBoard.css";
 
-
-export default function GameBoard({categories, questions, modalIsOpen, changeModalState}) {
-
-  const categoryList = categories.map((category, mapIndex) =>
-    <div key={mapIndex}>
+export default function GameBoard({
+  categories,
+  questions,
+  modalIsOpen,
+  changeModalState,
+}) {
+  const categoryList = categories.map((category, mapIndex) => (
+    <div className="oneCategoryRow" key={mapIndex}>
       <div className="category-cell">{category}</div>
-      {questions[mapIndex].map((question, questionIndex) =>
+      {questions[mapIndex].map((question, questionIndex) => (
         <div
           className="question-cell"
           key={questionIndex}
           onClick={() => changeModalState(question)}
-        > {question}:
-          {questionIndex * 100 + 100}{" "}
+        >
+          {" "}
+          {question}:{questionIndex * 100 + 100}{" "}
         </div>
-      )}
+      ))}
     </div>
-  );
+  ));
 
-  return (
-    <div className="game-board">
-      {categoryList}
-    </div>
-  );
-
-};
+  return <div className="game-board">{categoryList}</div>;
+}
